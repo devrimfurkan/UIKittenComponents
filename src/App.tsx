13d7b12988d08d184dc0c53d642281dc9612f19f
Devrim Furkan GUNER <70522562+devrimfurkan@users.noreply.github.com>
2022-07-25 11:11:49 +0300

    Icon component usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import Layout from './components/Layout';
 import Text from './components/Text';
 import Divider from './components/Divider';
+import Icon from './components/Icon';
 
 export default function App() {
   return (
-    <ApplicationProvider {...eva} theme={eva.light}>
+    <ApplicationProvider {...eva} theme={eva.light} >
+      <IconRegistry icons={EvaIconsPack}/>
       <Layout />
       <Text />
       <Divider/>
+      <Icon/>
     </ApplicationProvider>
   );
 };
